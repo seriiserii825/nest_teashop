fec4ed5cd2bf4849b20acbbb58dc58c8f6de9282
serii burduja <seriiburduja@gmail.com>
2026-01-23 18:15:54 +0200

    feat: app module connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,27 @@
 import { Module } from '@nestjs/common';
 
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        // Для production используйте миграции
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+        migrationsRun: false, // Автоматически запускать миграции
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
