6edbbc2dca3f239df0b45c8ac3f6aa89cf513611
serii <seriiburduja@gmail.com>
2026-01-23 21:04:07 +0200

    feat: store entitty

--- src/store/entities/store.entity.ts
+++ src/store/entities/store.entity.ts
@@ -1,1 +1,25 @@
-export class Store {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('stores')
+export class Store {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  string: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
