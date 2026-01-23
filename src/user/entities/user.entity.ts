e5cb1722f79b1bd50f52a252a72f9445a0633007
serii <seriiburduja@gmail.com>
2026-01-23 20:15:41 +0200

    feat: migration for user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,9 +1,11 @@
 import {
+  Entity,
   CreateDateColumn,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
+@Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
