14ec64e7e21e762dd6953f543e2d29194c8febc6
serii <seriiburduja@gmail.com>
2026-01-23 20:02:02 +0200

    feat: create user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class User {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+}
