1623a829b664c3b5536cc46f677ec56a77a38f7b
serii <seriiburduja@gmail.com>
2026-01-23 22:25:52 +0200

    feat: cookie parser

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,24 @@ import { AppModule } from './app.module';
 import validationConfig from './config/validation.confit';
 import swaggerConfig from './config/swagger.config';
 
+import cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   validationConfig(app);
   swaggerConfig(app);
+  app.use(cookieParser());
   app.enableCors({
-    origin: process.env.NODE_ENV === 'production' ? 'https://example.com' : '*',
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'https://example.com'
+        : process.env.CLIENT_URL_DEVELOPMENT,
     credentials: true,
+    exposeHeaders: ['set-cookie'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error during application bootstrap:', err);
+});
