d87fa9e31d6599274986fd36b76b6f541d65b570
serii <seriiburduja@gmail.com>
2026-01-24 10:26:29 +0200

    feat: jwt strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from 'src/user/user.service';
+import { IJwtPayload } from '../interfaces/IJwtPayload';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    private userService: UserService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow('JWT_SECRET'),
+    });
+  }
+
+  validate(payload: IJwtPayload) {
+    return this.userService.findOne(payload.id);
+  }
+}
