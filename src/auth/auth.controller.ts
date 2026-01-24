762a89ec2a7471113998b6fed9625c7f1bb3690d
serii <seriiburduja@gmail.com>
2026-01-24 16:41:07 +0200

    feat: auth controller google links

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
 import { TypedBody, TypedRoute } from '@nestia/core';
-import { BadGatewayException, Controller, Req, Res } from '@nestjs/common';
+import {
+  BadGatewayException,
+  Controller,
+  HttpCode,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import type { Request, Response } from 'express';
 import type { TCreateUserDto, TUserResponseDto } from 'src/user/dto/user.dto';
-import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { ILoginResponseWithoutRefresh } from './interfaces/ILoginResponse';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly userService: UserService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @TypedRoute.Post('register')
   create(@TypedBody() input: TCreateUserDto): Promise<TUserResponseDto> {
@@ -43,5 +47,31 @@ export class AuthController {
     this.authService.addRefreshTokenToResponse(res, tokens.refreshToken);
 
     return { ...user, tokens: { accessToken: tokens.accessToken } };
+  }
+
+  @HttpCode(200)
+  @TypedRoute.Post('logout')
+  logout(@Res({ passthrough: true }) res: Response): void {
+    this.authService.removeRefreshTokenFromResponse(res);
+  }
+
+  @UseGuards(AuthGuard('google'))
+  @TypedRoute.Get('google')
+  googleAuth(@Req() _req: Request) {
+    console.log('Google auth', _req.user);
+  }
+
+  @UseGuards(AuthGuard('google'))
+  @TypedRoute.Get('google/callback')
+  async googleAuthCallback(
+    @Req() req: { user: { name: string; email: string; picture: string } },
+    @Res() res: Response,
+  ) {
+    const { tokens } = await this.authService.validateOAuthLogin(req);
+    this.authService.addRefreshTokenToResponse(res, tokens.refreshToken);
+
+    return res.redirect(
+      `${process.env.CLIENT_URL}/dashboard?accessToken=${tokens.accessToken}`,
+    );
   }
 }
