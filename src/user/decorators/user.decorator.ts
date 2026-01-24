58921086a8be9e9fd26370c278f14b4128a484e6
serii <seriiburduja@gmail.com>
2026-01-24 16:43:46 +0200

    feat: user decorator

---
+++ src/user/decorators/user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+
+export const CurrentUser = createParamDecorator(
+  (data: keyof User, ctx: ExecutionContext) => {
+    const request: { user: User } = ctx.switchToHttp().getRequest();
+    const user = request.user;
+    return data ? user?.[data] : user;
+  },
+);
