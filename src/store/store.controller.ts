e278447706f821b9daba25ff3fda5724182b02a5
serii <seriiburduja@gmail.com>
2026-01-24 19:58:23 +0200

    feat: store crud dto swagger

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiBody, ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 import { AuthJwt } from 'src/auth/decorators/auth.jwt.decorator';
 import { CurrentUser } from 'src/user/decorators/user.decorator';
 import { User } from 'src/user/entities/user.entity';
@@ -25,11 +26,14 @@ export class StoreController {
   constructor(private readonly storeService: StoreService) {}
 
   @Post()
+  @ApiBody({ type: CreateStoreDto })
+  @ApiCreatedResponse({ type: StoreResponseDto })
   create(@CurrentUser() user: User, @Body() input: CreateStoreDto) {
     return this.storeService.create(input, +user.id);
   }
 
   @Get()
+  @ApiOkResponse({ type: [StoreResponseDto] })
   async findAll(@CurrentUser('id') id: number): Promise<StoreResponseDto[]> {
     const stores = await this.storeService.findAll(id);
     return stores.map((store) => {
@@ -38,11 +42,14 @@ export class StoreController {
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: StoreResponseDto })
   findOne(@CurrentUser() user: User, @Param('id') id: string) {
     return this.storeService.findOne(+id, user.id);
   }
 
   @Patch(':id')
+  @ApiBody({ type: UpdateStoreDto })
+  @ApiOkResponse({ type: StoreResponseDto })
   update(
     @CurrentUser() user: User,
     @Param('id') id: string,
@@ -52,6 +59,7 @@ export class StoreController {
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: StoreRemoveResponseDto })
   remove(
     @CurrentUser() user: User,
     @Param('id') id: string,
