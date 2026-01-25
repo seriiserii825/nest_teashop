451a4c906129fc35fa79ced11da1105ba8ce10da
serii <seriiburduja@gmail.com>
2026-01-25 13:48:36 +0200

    feat: product dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,62 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsNumber, IsString } from 'class-validator';
+
+class Product {
+  @ApiProperty({ example: 1, description: 'Product ID' })
+  @IsNumber()
+  id: number;
+
+  @ApiProperty({ example: 'Green Tea', description: 'Product Title' })
+  @IsString()
+  title: string;
+
+  @ApiProperty({
+    example: 'A refreshing green tea.',
+    description: 'Product Description',
+  })
+  @IsString()
+  description: string;
+
+  @ApiProperty({ example: 9.99, description: 'Product Price' })
+  @IsNumber()
+  price: number;
+
+  @ApiProperty({
+    example: 'http://example.com/images/green-tea.jpg',
+    description: 'Product Images URL',
+  })
+  @IsString()
+  images: string;
+
+  @ApiProperty({ example: 1, description: 'Store ID' })
+  @IsNumber()
+  store_id: number;
+
+  @ApiProperty({ example: 2, description: 'Category ID' })
+  @IsNumber()
+  category_id: number;
+
+  @ApiProperty({ example: 3, description: 'Color ID' })
+  @IsNumber()
+  color_id: number;
+
+  @ApiProperty({ example: 4, description: 'User ID' })
+  @IsNumber()
+  user_id: number;
+
+  @ApiProperty({
+    example: '2024-01-01T00:00:00Z',
+    description: 'Creation Date',
+  })
+  @IsDate()
+  createdAt: Date;
+
+  @ApiProperty({
+    example: '2024-01-02T00:00:00Z',
+    description: 'Last Update Date',
+  })
+  @IsDate()
+  updatedAt: Date;
+}
+
+export class CreateProductDto extends Product {}
